bf8599a85749c2b745d826fe0e7e45ffa6341e22
mirzahaifzh <mirzafio78@gmail.com>
2024-09-19 12:23:12 +0800

    Initial commit

---
+++ app.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const userRoutes = require('./routes/userRoutes'); // Import routes yang berisi controller user
+const laporanRoutes = require('./routes/laporanRoutes'); // Import routes yang berisi controller user
+const sequelize = require('./config/config'); // Import konfigurasi database Sequelize
+const laporan = require('./models/laporan');
+
+const app = express();
+
+// Middleware
+app.use(cors()); // Mengizinkan CORS (opsional, bisa disesuaikan dengan kebutuhan)
+app.use(bodyParser.json()); // Parsing JSON request
+app.use(bodyParser.urlencoded({ extended: true })); // Parsing URL-encoded request
+
+// Routes
+app.use('/api', userRoutes); // Semua route user akan diawali dengan '/api'
+app.use('/api', laporanRoutes); // Semua route
+
+// Route untuk halaman utama (bisa diubah sesuai kebutuhan)
+app.get('/', (req, res) => {
+  res.send('Welcome to the User Management API');
+});
+
+// Handle error jika route tidak ditemukan
+app.use((req, res, next) => {
+  res.status(404).json({
+    message: 'Endpoint not found!'
+  });
+});
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({
+    message: 'An unexpected error occurred!',
+    error: err.message
+  });
+});
+
+// Start server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
