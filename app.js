0c77aee346f5335b30cccd385b025a6f80ab42a6
mirzahaifzh <mirzafio78@gmail.com>
2025-03-01 19:08:38 +0800

    fix

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const app = express();
 const corsOptions = {
   origin: [
     'https://renisanawasena.online', // Production domain
-    'http://localhost:5173' // Local development domain
+    'http://localhost:5173','https://renisa-report-web.vercel.app' // Local development domain
   ],
   methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed HTTP methods
   allowedHeaders: ['Content-Type', 'Authorization'], // Allowed headers
